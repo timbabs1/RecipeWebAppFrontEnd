b500c0dacb96c6d676ce3478b31035bdcb14057e
Babs <Haastrup>
2019-11-07 15:19:05 +0000

    signup_functionality_v2

---
+++ src/components/ingredients.js
@@ -0,0 +1,174 @@
+import React from 'react';
+
+import { Form, Input, Icon, Button, Alert } from 'antd';
+
+let id = 0;
+
+class IngredientsForm extends React.Component {
+
+  state = {
+    confirmDirty: false,
+    ingredientAddedSuccessfully: false, //if the user is added successfully
+    showLogin: false, //if should we show a succesful feedback message after loggin in a user
+    showError: false, //if should we show an error feedback message after logging in a user
+    errorCode: 400, //to save the errorCode we received from the api server
+    responseStatus: "warning",
+    errorMessage: "", //the error message to display to the user after server rejects action
+    username: this.props.username,
+    password: this.props.password,
+    recipeId: this.props.recipeId
+  };
+  remove = k => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    // We need at least one passenger
+    if (keys.length === 1) {
+      return;
+    }
+
+    // can use data-binding to set
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  };
+
+  add = () => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    const nextKeys = keys.concat(id++);
+    // can use data-binding to set
+    // important! notify form to detect changes
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const { keys, names } = values;
+        console.log('Received values of form: ', values);
+        console.log('Merged values:', keys.map(key => names[key]));
+        console.log(values.names)
+        fetch('http://localhost:3000/api/v1.0/ingredient',{
+          method: 'POST',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': 'Basic ' + window.btoa(this.state.username + ':' + this.state.password),
+          },
+          body: {form_value: JSON.stringify({values}), recipe_Id: this.state.recipeId}
+          }).then(res => {
+            if(res.ok){
+                this.setState({ingredientAddedSuccessfully:true})
+                return res.json()
+            }else {
+                console.log(res.status)
+                this.setState({
+                    recipeAddedSuccessfully:false,
+                    errorCode: res.status,
+                    errorMessage: res.statusText 
+                });
+                
+                return res.json()
+            }
+        }).then(data => this.checkResponse(data))
+      }
+    });
+  };
+
+  checkResponse = (data) => {
+
+    if(this.state.recipeAddedSuccessfully){
+        this.props.form.resetFields();
+        this.setState({
+            showSuccess:true,
+            showError : false,
+            responseStatus: "success"
+        });
+    } else {
+        //handle errors
+        this.setState({
+            errorMessage: data.message,
+            showSuccess:false,
+            showError:true,
+            responseStatus:"error"
+        });
+    }
+  }
+
+  render() {
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 },
+      },
+    };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 20, offset: 4 },
+      },
+    };
+    getFieldDecorator('keys', { initialValue: [] });
+    const keys = getFieldValue('keys');
+    const formItems = keys.map((k, index) => (
+      <Form.Item
+        {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+        label={index === 0 ? 'Ingredients ' : ''}
+        required={false}
+        key={k}
+      >
+        {getFieldDecorator(`names[${k}]`, {
+          validateTrigger: ['onChange', 'onBlur'],
+          rules: [
+            {
+              required: true,
+              whitespace: true,
+              message: "Please input Ingredient name or delete this field.",
+            },
+          ],
+        })(<Input placeholder="ingredient name" style={{ width: '60%', marginRight: 8 }} />)}
+        {keys.length > 1 ? (
+          <Icon
+            className="dynamic-delete-button"
+            type="minus-circle-o"
+            onClick={() => this.remove(k)}
+          />
+        ) : null}
+      </Form.Item>
+    ));
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        {formItems}
+        <Form.Item {...formItemLayoutWithOutLabel}>
+          <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
+            <Icon type="plus" /> Add field
+          </Button>
+        </Form.Item>
+        <Form.Item {...formItemLayoutWithOutLabel}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+        {this.state.showSuccess ? <Alert message="ingredient added successfully" type="success" /> :null}
+        {this.state.showError ? <Alert message={"Error code " + this.state.errorCode + " " + this.state.errorMessage} type="error" /> :null}
+      </Form>
+    );
+  }
+}
+
+const Ingredients = Form.create({ name: 'ingredients' })(IngredientsForm);
+export default Ingredients;
+//ReactDOM.render(<WrappedDynamicFieldSet />, mountNode);
+
+/* const WrappedDynamicFieldSet = Form.create({ name: 'dynamic_form_item' })(DynamicFieldSet);
+ReactDOM.render(<WrappedDynamicFieldSet />, mountNode); */
--- src/components/recipe.js
+++ src/components/recipe.js
@@ -1,29 +1,30 @@
 import React from 'react';
 
-
-
 import {
     Form,
     Input,
     Alert,
-    Card,
-    Row,
     Checkbox,
-    Button
+    Button,
+    Select
 } from 'antd';
 import { relativeTimeThreshold } from 'moment';
-import { valueToNode } from '@babel/types';
 
-class LoginForm extends React.Component {
+const { Option } = Select;
+
+class RecipeForm extends React.Component {
 
     state = {
         confirmDirty: false,
-        loggedInSuccessfully: false, //if the user is added successfully
-        showSuccess: false, //if should we show a succesful feedback message after loggin in a user
+        recipeAddedSuccessfully: false, //if the user is added successfully
+        showLogin: false, //if should we show a succesful feedback message after loggin in a user
         showError: false, //if should we show an error feedback message after logging in a user
         errorCode: 400, //to save the errorCode we received from the api server
-        responseStatus: "nothing", //the validation status of the email
+        responseStatus: "warning",
         errorMessage: "", //the error message to display to the user after server rejects action
+        username: this.props.username,
+        password: this.props.password,
+        recipeId: 48
     };
 
     handleSubmit = e => {
@@ -33,48 +34,50 @@ class LoginForm extends React.Component {
                 //echo the values to the browswer console to make sure they are correct
                 console.log('Received values of form: ', values);
 
+                console.log(this.state.username)
+
                 //here we should send a request to our server
                 //use fetch API to post the user data
 
-                fetch('http://localhost:3000/api/v1.0/login',{
+                fetch('http://localhost:3000/api/v1.0/recipe',{
                     method: 'POST',
                     headers: {
                         'Accept': 'application/json',
                         'Content-Type': 'application/json',
-                        'Authorization': 'Basic ' + window.btoa(values.username + ':' + values.password),
+                        'Authorization': 'Basic ' + window.btoa(this.state.username + ':' + this.state.password),
                     },
                     body: JSON.stringify({values})
                 }).then(res => {
                     if(res.ok){
-                        this.setState({loggedInSuccessfully:true})
-                        this.props.view({
-                            username: values.username,
-                            password: values.password
-                        })
+                        this.setState({recipeAddedSuccessfully:true})
+                        return res.json()
                     }else {
                         console.log(res.status)
                         this.setState({
-                            loggedInSuccessfully:false,
+                            recipeAddedSuccessfully:false,
                             errorCode: res.status,
-                            showError: true,
-                            errorMessage: res.statusText
+                            errorMessage: res.statusText 
                         });
-
                         return res.json()
                     }
-                }).then(data => this.checkResponse(data))
+                    }).then(data => this.checkResponse(data))
             }
         })
     };
 
     checkResponse = (data) => {
 
-        if(this.state.loggedInSuccessfully){
-            //this.props.form.resetFields();
+        if(this.state.recipeAddedSuccessfully){
+            this.props.form.resetFields();
             this.setState({
                 showSuccess:true,
-                showError : false
+                showError : false,
+                responseStatus: "success",
+                recipeId: data.recipeData
             });
+            this.props.view({
+                recipeId: this.state.recipeId
+            })
         } else {
             //handle errors
             this.setState({
@@ -112,49 +115,71 @@ class LoginForm extends React.Component {
                 },
             },
         };
+        const config = {
+            rules: [
+              { type: "object", required: true, message: "Please select time!" }
+            ]
+        };
 
         return (
-            <Row type="flex" justify="space-aroud" align="center">
-            <Card title="Login" align="center" style={{ width: 420}}  >
             <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-                <Form.Item label="Username" hasFeedback validateStatus={this.state.responseStatus}>
-                    {getFieldDecorator('username', {
+                <Form.Item label="Title" hasFeedback validateStatus={this.state.responseStatus}>
+                    {getFieldDecorator('title', {
                         rules: [
-                            {
+                            /* {
                                 required: true,
-                                message: 'Please input your Username!',
-                            },
+                                message: 'Please input your title',
+                            }, */
                         ],
                     })(<Input onChange={this.handleEmail} />)}
                 </Form.Item>
-                <Form.Item label="Password" hasFeedback  validateStatus={this.state.responseStatus}>
-                    {getFieldDecorator('password', {
+                <Form.Item label="Subtitle" hasFeedback  validateStatus={this.state.responseStatus}>
+                    {getFieldDecorator('subtitle', {
                         rules: [
                             {
                                 required: true,
-                                message: 'Please input your password',
-                            },
-                            {
-                                min: 6,
-                                message: 'password should be at least 6 characters long!',
+                                message: 'Please input your subtitle',
                             },
                         ],
-                    })(<Input.Password />)}
+                    })(<Input />)}
+                </Form.Item>
+                <Form.Item label="Description" hasFeedback  validateStatus={this.state.responseStatus}>
+                    {getFieldDecorator('description', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Please input your description',
+                            },
+                        ],
+                    })(<Input />)}
+                </Form.Item>
+                <Form.Item label="CategoryId" hasFeedback>
+                    {getFieldDecorator('categoryId', {
+                        rules: [{ required: true, message: "Please select your recipe's category!" }],
+                    })(
+                        <Select placeholder="Please select a category">
+                        <Option value="Breakfast">Breakfast</Option>
+                        <Option value="Lunch">Lunch</Option>
+                        <Option value="Beverages">Beverages</Option>
+                        <Option value="Appetizers">Appertizers</Option>
+                        <Option value="Soups">Soups</Option>
+                        <Option value="Salads">Salads</Option>
+                        <Option value="Desserts">Desserts</Option>
+                        </Select>,
+                    )}
                 </Form.Item>
                 <Form.Item {...tailFormItemLayout}>
                     <Button type="primary" htmlType="submit">
-                        Login
+                        Submit
                     </Button>
                 </Form.Item>
-                {this.state.showSuccess ? <Alert message="account created successfully" type="success" /> :null}
+                {this.state.showSuccess ? <Alert message="recipe was added successfully" type="success" /> :null}
                 {this.state.showError ? <Alert message={"Error code " + this.state.errorCode + " " + this.state.errorMessage} type="error" /> :null}
             </Form>
-            </Card>
-            </Row>
         );
     }
 }
 
-const Login = Form.create({ name: 'login' })(LoginForm);
+const Recipe = Form.create({ name: 'recipe' })(RecipeForm);
 
-export default Login;
+export default Recipe;
