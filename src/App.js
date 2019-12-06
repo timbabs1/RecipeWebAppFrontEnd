2030fef3a43e1460dea02e0b19caaf75b5286c11
Francis <okaforf2@uni.coventry.ac.uk>
2019-12-06 14:55:40 +0000

    search UI - API Integration

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import logo from './logo.svg';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import Hello from './components/Hello';
-import Recipe from './components/recipe';
-import Ingredients from './components/ingredients';
-import Steps from './components/steps';
+import Recipe from './components/Recipe';
+import Ingredients from './components/Ingredients';
+import Steps from './components/Steps'
+import ReceipePreview from './components/RecipePreview'
 import './App.css';
 import { thisExpression } from '@babel/types';
+import RecipePreview from './components/RecipePreview';
+import Search from './components/search';
 
 class App extends React.Component {
   constructor(props){
@@ -72,52 +75,55 @@ class App extends React.Component {
   } */
 
   render() {
-    let whatToRender;
+//     let whatToRender;
 
-    if(this.state.currentView === 'home'){
-      whatToRender= <div> 
-                      <Hello name=" Babz" /> 
-                      <Recipe view={this.changeView2.bind(this)} username={this.state.username} password={this.state.password}/> 
-                      {/* <Login view={this.changeView.bind(this)}/> */}
-                    </div>
-      //TODO
-    }
+//     if(this.state.currentView === 'home'){
+//       whatToRender= <div> 
+//                       <Hello name=" Babz" /> 
+//                       <Recipe view={this.changeView2.bind(this)} username={this.state.username} password={this.state.password}/> 
+//                       {/* <Login view={this.changeView.bind(this)}/> */}
+//                     </div>
+//       //TODO
+//     }
 
-    else if(this.state.currentView === "ingredients"){
-      whatToRender = <Ingredients view={this.changeView3.bind(this)} categoryId={this.state.categoryId} recipeId={this.state.recipeId} username={this.state.username} password={this.state.password}/>
-    }
+//     else if(this.state.currentView === "ingredients"){
+//       whatToRender = <Ingredients view={this.changeView3.bind(this)} categoryId={this.state.categoryId} recipeId={this.state.recipeId} username={this.state.username} password={this.state.password}/>
+//     }
 
-    else if(this.state.currentView === "steps"){
-      whatToRender = <Steps view={this.changeView4.bind(this)} recipeId={this.state.recipeId} username={this.state.username} password={this.state.password}/>
-    }
+//     else if(this.state.currentView === "steps"){
+//       whatToRender = <Steps view={this.changeView4.bind(this)} recipeId={this.state.recipeId} username={this.state.username} password={this.state.password}/>
+//     }
 
-    else if(this.state.currentView === "login"){
-      whatToRender = 
-      <div>
-      <Router>
-          <div className="PageSwitcher">
-          <NavLink to="/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
-          <NavLink exact to="/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
-          </div>   
-          <div>
-          <Route path="/login" render={props => (<Login {...props} view={this.changeView.bind(this)}/>)}/>
-          <Route path="/Signup" render={props => (<Signup {...props} view={this.changeView.bind(this)}/>)}/>
-          <Redirect exact from="/" to="/login" />
-          </div>
-       </Router>
-      </div> 
-    }
+//     else if(this.state.currentView === "login"){
+//       whatToRender = 
+//       <div>
+//       <Router>
+//           <div className="PageSwitcher">
+//           <NavLink to="/login" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Login</NavLink>
+//           <NavLink exact to="/signup" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+//           </div>   
+//           <div>
+//           <Route path="/login" render={props => (<Login {...props} view={this.changeView.bind(this)}/>)}/>
+//           <Route path="/Signup" render={props => (<Signup {...props} view={this.changeView.bind(this)}/>)}/>
+//           <Redirect exact from="/" to="/login" />
+//           </div>
+//        </Router>
+//       </div> 
+//     }
 
-    else if(this.state.currentView === "hello"){
-      whatToRender = <Hello name='Babz'/>
-    }
+//     else if(this.state.currentView === "hello"){
+//       whatToRender = <Hello name='Babz'/>
+//     }
     /* else if(this.state.currentView === "signup"){
       whatToRender = <Signup view={this.changeView.bind(this)} />
     } */
     return (
 
     <div>
-      {whatToRender}
+      {/* {whatToRender} */}
+      {/* <RecipePreview /> */}
+      <Search />
+
     </div>
     );   
     }
---
+++ src/components/search/index.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import "./style.css";
+// import Popup from "./popup";
+import axios from 'axios';
+import { object } from "prop-types";
+import { Result } from "antd";
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      results: {},
+      message: '',
+    }
+      this.cancel = '';
+  };
+
+  handleOnInputChange = ( event ) => {
+      const query = event.target.value;
+      this.setState( { query: query, message: '' }, ()=> {
+          this.fetchSearchResults(query);
+      }); 
+  };
+
+
+fetchSearchResults = (query) => {
+    const searchUrl =  `https://api.edamam.com/search?q=${query}&app_id=e8eee3ca&app_key=9f37ced39b6203d7334bd4792f241dd4`
+
+    if( this.cancel ) {
+        this.cancel.cancel();
+    }
+
+    this.cancel = axios.CancelToken.source();
+    
+    axios.get( searchUrl, {
+      cancelToken: this.cancel.token
+    })
+        .then ( res => {
+          const resultNotFoundMsg = ! res.data.hits.length
+                                  ? 'No more search results'
+                                  : '';
+          this.setState( {
+            results: res.data.hits,
+            message: resultNotFoundMsg
+          })  
+                        
+        })
+    .catch( error => {
+      if ( axios.isCancel(error) || error) {
+          this.setState( {
+            message: 'AY! that was a mis-take'
+          })
+
+      }
+    })
+  }
+  
+
+  renderSearchResults = () => {
+    const { results } = this.state;
+
+    if( Object.keys( results ).length && results.length ) {
+      return (
+        <div className="results-container">
+          { results.map( result => {
+            return(
+              <a key={result.uri} href={ result.recipe.url } className="result-item">
+                <div className="image-wrapper"></div>
+                <img className="image" src={ result.recipe.image } alt={`${null}`}/>
+                
+
+              </a>
+            )
+          })}
+
+
+        </div>
+      )
+
+    }
+  }
+
+  render() {
+    const { query } = this.state;
+    
+    return (
+      <div className="search">
+        <div className="search-container">
+          <div className="title">Search Recipe</div>
+          <div className="content">
+            <input
+              type="text"
+              value={query}
+              name="query"
+              placeholder="Search..."
+              onChange={this.handleOnInputChange}
+//              onInput={this.onInput.bind(this)}
+            />
+            {/* <Popup isOpen={showResult} items={foundFoods} />  */}
+            {this.renderSearchResults()}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Search
